6a57471b88e49244b6724ba8080e22bf42e3a304
bishalacharya05 <bishalacharya05@gmail.com>
2025-05-09 01:49:53 +0545

    edit home page and added details view page for the product

--- OnlineStore/Areas/Customer/Controllers/HomeController.cs
+++ OnlineStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using OnlineStore.DataAcess.Repository;
+using OnlineStore.DataAcess.Repository.IRepository;
 using OnlineStore.Models;
 
 namespace OnlineStore.Areas.Customer.Controllers
@@ -8,16 +10,25 @@ namespace OnlineStore.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
+
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productsList= _unitOfWork.Product.GetAll(includProperties: "Category").ToList();
+            return View(productsList);
         }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u =>u.ProductId == productId,includProperties: "Category");
+            return View(product);
+        }
+
 
         public IActionResult Privacy()
         {
